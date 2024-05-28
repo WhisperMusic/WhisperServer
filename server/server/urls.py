bc5c4015b5d841a035e8cb488ae320aa3f4f77d5
Soucelover <mr.ktotov@gmail.com>
2024-05-28 17:13:50 +0300

    Add `api` application URLS to `urls.py`

--- server/server/urls.py
+++ server/server/urls.py
@@ -16,13 +16,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework import routers
+
+from server.api import views
 
 # Firstly root URI will be REST API's view
 # for now "api/*" will be just for requests to server
 
 # TODO: Move REST API's view to smth like "api/doct/"
 
+router = routers.DefaultRouter()
+router.register("users", views.UserViewSet)
+router.register("groups", views.GroupViewSet)
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("rest_framework.urls", namespace="rest_framework")),
+    path("", include(router.urls)),
+    path(
+        "api-auth/",
+        include("rest_framework.urls", namespace="rest_framework"),
+    ),
 ]
