79edd480d2089231f2b7f1a5486786b54af55838
Soucelover <mr.ktotov@gmail.com>
2024-05-28 18:39:15 +0300

    Use `python-dotenv` to load server's environment

--- server/server/settings.py
+++ server/server/settings.py
@@ -12,12 +12,16 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+from dotenv import find_dotenv, load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+
+load_dotenv(find_dotenv(raise_error_if_not_found=True))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = (
