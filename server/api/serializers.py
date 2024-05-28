9bcb73522ea81f08f0077fff244cfcd9aa776c40
Soucelover <mr.ktotov@gmail.com>
2024-05-28 17:06:04 +0300

    Write basic serializers

---
+++ server/api/serializers.py
@@ -0,0 +1,15 @@
+from typing import ClassVar
+
+from django.contrib.auth.models import Group, User
+from rest_framework.serializers import HyperlinkedModelSerializer
+
+
+class UserSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields: ClassVar = ["urls", "username", "email", "groups"]
+
+class GroupSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields: ClassVar = ["url", "name"]
