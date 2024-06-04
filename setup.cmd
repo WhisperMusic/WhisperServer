59762f12b30c6a37e20d61cc4cde4b92b35f139f
Soucelover <mr.ktotov@gmail.com>
2024-06-04 16:04:30 +0300

    Split `setup.cmd` into functions
    In non-splitted script in if clauses with multiple statements it's not
    possible to set env variable without & separation.

--- setup.cmd
+++ setup.cmd
@@ -1,20 +1,27 @@
 @echo off
+goto start
 
-if not defined VIRTUAL_ENV (
+:SETUP_VIRTUAL_ENVIRONMENT
     echo You are running setup without Python Virtual Environment activated
     choice /c YN /m "Do you want to automatically create new environment"
 
-    if errorlevel == 1 (
-        set /p _venv_path="Path to Virtual Environment [default=.venv]: "
+    if errorlevel == 1 call :CREATE_VIRTUAL_ENVIRONMENT
+    goto :eof
 
-        if not defined _venv_path set _venv_path=.venv
+:CREATE_VIRTUAL_ENVIRONMENT
+    set /p _venv_path="Path to Virtual Environment [default=.venv]: "
 
-        echo .
-        echo Creating Virtual Environment . . . 
-        echo .
 
-        python -m venv %_venv_path% && %_venv_path%\Scripts\activate && pip install -r requirements.txt
+    if not defined _venv_path set _venv_path=.venv
 
-        echo .
-    )
-)
+    echo.
+    echo Creating Virtual Environment . . . 
+    echo.
+
+    python -m venv %_venv_path% && %_venv_path%\Scripts\activate && pip install -r requirements.txt
+
+    echo.
+    goto :eof
+
+:start
+if not defined VIRTUAL_ENV call :SETUP_VIRTUAL_ENVIRONMENT
