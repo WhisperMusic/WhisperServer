be20d41b7d949906cf784c805b65395254e14076
Soucelover <mr.ktotov@gmail.com>
2024-06-05 17:40:23 +0300

    Add `verbose_name` to config of `api`

--- server/api/apps.py
+++ server/api/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ApiConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"  # type: ignore
     name = "api"
+    verbose_name = "Muse Store Server REST API"
