0e2fd143b98a3f721dfda8b2c4e60c509756aefe
Soucelover <mr.ktotov@gmail.com>
2024-06-06 18:16:29 +0300

    Add `artist` field

--- server/muse_store/tracks/models.py
+++ server/muse_store/tracks/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class TrackModel(models.Model):
     title = models.CharField(max_length=150)
+    artist = models.CharField(max_length=150)
 
     def __str__(self) -> str:
-        return self.title
+        return f"{self.artist} - {self.title}"
