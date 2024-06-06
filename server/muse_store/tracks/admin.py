f69d640a949f1c5f7485a21979a47cc73d4e0a8a
Soucelover <mr.ktotov@gmail.com>
2024-06-06 20:18:54 +0300

    Register `TrackModel` in admin panel

--- server/muse_store/tracks/admin.py
+++ server/muse_store/tracks/admin.py
@@ -1,1 +1,10 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import TrackModel
+
+
+class TrackAdmin(admin.ModelAdmin):
+    fields = ["title", "artist"]  # noqa: RUF012
+
+
+admin.site.register(TrackModel, TrackAdmin)
