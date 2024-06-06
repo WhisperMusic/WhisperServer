8dec63e2e1e68a59c406fc042d11182ec8e88613
Soucelover <mr.ktotov@gmail.com>
2024-06-06 18:14:59 +0300

    Write simple `TrackModel`

--- server/muse_store/tracks/models.py
+++ server/muse_store/tracks/models.py
@@ -1,1 +1,8 @@
-# Create your models here.
+from django.db import models
+
+
+class TrackModel(models.Model):
+    title = models.CharField(max_length=150)
+
+    def __str__(self) -> str:
+        return self.title
