8bbd5032a14a3723ba8c458dbdddabbfafdcd62c
Soucelover <mr.ktotov@gmail.com>
2024-06-06 18:30:12 +0300

    Add simple viewset for `TrackModel`

--- server/muse_store/tracks/views.py
+++ server/muse_store/tracks/views.py
@@ -1,1 +1,10 @@
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+
+from muse_store.tracks.models import TrackModel
+from muse_store.tracks.serializers import TrackModelSerializer
+
+
+class TrackModelViewSet(ModelViewSet):
+    queryset = TrackModel.objects.all()
+    serializer_class = TrackModelSerializer
+    # TODO: Add permission classes to viewset
