6de49b5498ba545c1e6018010ae3f3c84ebc9072
Soucelover <mr.ktotov@gmail.com>
2024-06-06 16:44:23 +0300

    Add type ignores on assignment

--- server/muse_store/apps.py
+++ server/muse_store/apps.py
@@ -2,6 +2,6 @@ from django.apps import AppConfig
 
 
 class MuseServerConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"  # type: ignore[reportAssignmentType]
+    default_auto_field = "django.db.models.BigAutoField"  # pyright: ignore[reportAssignmentType]
     name = "muse_store"
     verbose_name = "Muse Store Server's API"
--- server/muse_store/users/apps.py
+++ server/muse_store/users/apps.py
@@ -2,6 +2,6 @@ from django.apps import AppConfig
 
 
 class UsersConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
+    default_auto_field = "django.db.models.BigAutoField"  # pyright: ignore [reportAssignmentType]
     name = "users"
     verbose_name = "Muse Store Server API models related to users"
