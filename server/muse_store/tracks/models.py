11d4513c5c9ceefaf36bf08253eaaa1412ee63d9
Soucelover <mr.ktotov@gmail.com>
2024-06-07 13:42:17 +0300

    Add `uploader` field

--- server/muse_store/tracks/models.py
+++ server/muse_store/tracks/models.py
@@ -1,5 +1,6 @@
 from typing import ClassVar, Self
 
+from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.manager import BaseManager
 
@@ -7,7 +8,11 @@ from django.db.models.manager import BaseManager
 class Track(models.Model):
     title = models.CharField(max_length=150)
     artist = models.CharField(max_length=150)
+
     audio = models.FileField(upload_to="audio/")
+
+    uploader = models.ForeignKey(User, models.CASCADE)
+
     objects: ClassVar[BaseManager[Self]]
 
     def __str__(self) -> str:
