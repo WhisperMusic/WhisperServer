c76bcbfab054067df0d11a4a329ea109bdc7891b
Soucelover <mr.ktotov@gmail.com>
2024-06-07 11:58:21 +0300

    Configure storage in settings

--- server/server/settings.py
+++ server/server/settings.py
@@ -140,6 +140,12 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+# Files uploaded by users
+
+MEDIA_ROOT = BASE_DIR / "media/"
+
+MEDIA_URL = "media/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
