05082071fcfbbac26c713d7e5619197ee11ef901
Soucelover <mr.ktotov@gmail.com>
2024-06-07 13:05:30 +0300

    Rename `Track` viewset

--- server/muse_store/tracks/views.py
+++ server/muse_store/tracks/views.py
@@ -4,7 +4,7 @@ from muse_store.tracks.models import Track
 from muse_store.tracks.serializers import TrackSerializer
 
 
-class TrackModelViewSet(ModelViewSet):
+class TrackViewSet(ModelViewSet):
     queryset = Track.objects.all()
     serializer_class = TrackSerializer
     # TODO: Add permission classes to viewset
