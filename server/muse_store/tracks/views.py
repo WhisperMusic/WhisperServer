cae689ae26c4bde98e4c356e20444bbd037396f5
Soucelover <mr.ktotov@gmail.com>
2024-06-11 13:21:15 +0300

    Write doc-strings

--- server/muse_store/tracks/views.py
+++ server/muse_store/tracks/views.py
@@ -5,5 +5,10 @@ from muse_store.tracks.serializers import TrackSerializer
 
 
 class TracksViewSet(ReadOnlyModelViewSet):
+    """General tracks viewset.
+
+    This viewset provides read-only views for listing and retrieving tracks.
+    """
+
     queryset = Track.objects.all()
     serializer_class = TrackSerializer
