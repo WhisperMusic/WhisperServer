52b75d9c0fd88a23da4f7eb700cd6e9c19e8de4d
Soucelover <mr.ktotov@gmail.com>
2024-06-11 17:23:34 +0300

    Reduce `.retrieve` method

--- server/muse_store/tracks/views.py
+++ server/muse_store/tracks/views.py
@@ -1,10 +1,7 @@
 from typing import Any, override
 
 from django.db.models.manager import BaseManager
-from django.shortcuts import get_object_or_404
-from rest_framework.exceptions import NotAuthenticated, PermissionDenied
-from rest_framework.request import Request
-from rest_framework.response import Response
+from rest_framework.exceptions import NotAuthenticated
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 
 from muse_store.tracks.models import Track
@@ -44,18 +41,3 @@ class MyTracksViewSet(ModelViewSet):
     def check_logged_in(self) -> None:
         if not self.request.user.is_authenticated:
             raise NotAuthenticated
-
-    @override
-    def retrieve(
-        self, request: Request, pk: str | None = None,
-    ) -> Response:
-        self.check_logged_in()
-        queryset = Track.objects.all()
-        track = get_object_or_404(queryset, pk=pk)
-
-        if track.uploader != request.user:
-            msg = "This track is owned not by you"
-            raise PermissionDenied(msg)
-
-        serializer = self.get_serializer(track)
-        return Response(serializer.data)
