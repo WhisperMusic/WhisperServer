75789b2510bc61f57ce855fd7cf03651943a8917
Soucelover <mr.ktotov@gmail.com>
2024-06-11 14:42:33 +0300

    Override `.retrieve` method

--- server/muse_store/tracks/views.py
+++ server/muse_store/tracks/views.py
@@ -2,6 +2,7 @@ from typing import override
 
 from django.db.models.manager import BaseManager
 from django.http import HttpRequest
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
@@ -44,4 +45,23 @@ class MyTracksViewSet(ModelViewSet):
         queryset = self.get_queryset()
         serializer = self.get_serializer(queryset, many=True)
 
+        return Response(serializer.data)
+
+    @override
+    def retrieve(
+        self, request: HttpRequest, pk: str | None = None,
+    ) -> Response:
+        response = self.check_logged_in()
+
+        if response is not None:
+            return response
+
+        queryset = Track.objects.all()
+        track = get_object_or_404(queryset, pk=pk)
+
+        if track.uploader != request.user:
+            content = {"detail": "This track is owned not by you."}
+            return Response(content, status=status.HTTP_403_FORBIDDEN)
+
+        serializer = self.get_serializer(track)
         return Response(serializer.data)
