663f0faaeb5097916f46d9e2aab08062d121838f
Soucelover <mr.ktotov@gmail.com>
2024-06-13 13:37:12 +0300

    Modify `Track` model representation on admin site

--- server/muse_store/tracks/admin.py
+++ server/muse_store/tracks/admin.py
@@ -3,12 +3,13 @@ from django.contrib import admin
 from .models import Track
 
 
+@admin.register(Track)
 class TrackAdmin(admin.ModelAdmin):
     fieldsets = [  # noqa: RUF012
         (None, {"fields": ["artist", "title"]}),
+        ("Date information", {"fields": ["date_uploaded"]}),
         ("Audio file", {"fields": ["audio"]}),
         ("Uploader of track", {"fields": ["uploader"]}),
     ]
-
-
-admin.site.register(Track, TrackAdmin)
+    date_hierarchy = "date_last_modified"
+    list_display = ["title", "artist", "uploader", "date_uploaded"]  # noqa: RUF012
