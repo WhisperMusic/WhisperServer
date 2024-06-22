adb9540bccfafb22ef4c6d3fbae6fa96671f4aac
Soucelover <mr.ktotov@gmail.com>
2024-06-22 16:55:02 +0300

    Extract schema definition to separated file

---
+++ server/muse_store/schema.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title="Whisper REST API")
+
+urls = [
+    path("docs/", schema_view),
+]
--- server/muse_store/urls.py
+++ server/muse_store/urls.py
@@ -1,12 +1,10 @@
 from django.urls import include, path
-from rest_framework_swagger.views import get_swagger_view
 
+from . import schema
 from .router import router
-
-schema_view = get_swagger_view(title="Whisper REST API")
 
 urlpatterns = [
     *router.urls,
     path("auth/", include("rest_framework.urls", namespace="rest_framework")),
-    path("docs/", schema_view),
+    *schema.urls,
 ]
